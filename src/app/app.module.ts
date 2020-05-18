788f38c51719e9dbc7d939b3a4961b16ab33490c
BlaiseAddeh <addehblaise@yahoo.fr>
2020-05-18 10:24:30 +0000

    Day 1 -bootstrap and homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shop/header/header.component';
+import { SlideshowComponent } from './shop/slideshow/slideshow.component';
+import { RegistrationComponent } from './shop/registration/registration.component';
+import { ShoutoutComponent } from './shop/shoutout/shoutout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SlideshowComponent,
+    RegistrationComponent,
+    ShoutoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
